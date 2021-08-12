4b22572eacfd91c9eeea268b6caa676df656c4c5
Adam Graham <adam@zigurous.com>
2021-08-12 16:04:33 -0500

    Fix COA website link

--- src/pages/coa.js
+++ src/pages/coa.js
@@ -13,8 +13,9 @@ function Coa() {
       title="Chenga Ose Association"
       desciption={[
         'Chenga Ose Association, which means ‘all considered’, is a grassroots, Christian based organization. COA was formed in 2008 to build new hope and a future for those with disabilities and their families. COA’s scope reaches five provinces of Zimbabwe and directly cares for and empowers more than five hundred individuals with disabilities and their families. ',
-        <br />,
-        'https://chengaose.com/',
+        <a href="https://chengaose.com/" rel="noreferrer" target="_blank">
+          https://chengaose.com
+        </a>,
         <br />,
         <br />,
         'I designed a new logo for COA to establish a more professional looking brand. They have a special connection with a green pumpkin in their mythology and wanted that to be in their logo, so my approach was to subtly hint at this by making the letters round and arrange them to form the pumpkins base.',
